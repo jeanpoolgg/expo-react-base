75ddf13e4dfef6eba2658e3a0c2d384ed6bb7af2
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-21 18:22:36 -0500

    feat(context-api): add context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 // import { BasicTypes } from './typescript/BasicTypes'
 // import { ObjectLiterals } from './typescript/ObjectLiterals'
 // import { BasicFunctions } from './typescript/BasicFunctions'
-import { Counter } from "./components/Counter"
+// import { Counter } from "./components/Counter"
+import { LoginPage } from "./components/LoginPage"
+import { AuthProvider } from "./context/AuthContext"
 
 function App() {
 
   return (
     <>
-      <div className='flex flex-col justify-center items-center h-svh'>
-        <h1 className="text-4xl mb-5">React + TS</h1>
-        {/* <BasicTypes/> */}
-        {/* <ObjectLiterals/> */}
-        {/* <BasicFunctions /> */}
-        <Counter />
-      </div>
+      <AuthProvider>
+        <div className='flex flex-col justify-center items-center h-svh'>
+          <h1 className="text-4xl mb-5">React + TS</h1>
+          {/* <BasicTypes/> */}
+          {/* <ObjectLiterals/> */}
+          {/* <BasicFunctions /> */}
+          {/* <Counter /> */}
+          <LoginPage />
+        </div>
+      </AuthProvider>
     </>
   )
 }
