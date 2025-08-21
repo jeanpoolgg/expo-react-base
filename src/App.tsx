4ebc50f830973dfbb3d0533510ca77aebc4ea9d7
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-21 17:54:40 -0500

    feat(state): add counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 // import { BasicTypes } from './typescript/BasicTypes'
 // import { ObjectLiterals } from './typescript/ObjectLiterals'
 // import { BasicFunctions } from './typescript/BasicFunctions'
+import { Counter } from "./components/Counter"
 
 function App() {
 
   return (
     <>
-      <div className='flex justify-center items-center h-svh'>
+      <div className='flex flex-col justify-center items-center h-svh'>
         <h1 className="text-4xl mb-5">React + TS</h1>
+        {/* <BasicTypes/> */}
+        {/* <ObjectLiterals/> */}
+        {/* <BasicFunctions /> */}
+        <Counter />
       </div>
-      {/* <BasicTypes/> */}
-      {/* <ObjectLiterals/> */}
-      {/* <BasicFunctions /> */}
     </>
   )
 }
