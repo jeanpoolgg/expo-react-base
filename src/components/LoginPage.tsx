d3dab97cde72192f0e3e47a658ab186b0f9a93a3
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-21 18:43:21 -0500

    feat(context-api): add state management to context api

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,12 +2,15 @@ import { useAuthContext } from "../context/AuthContext"
 
 export const LoginPage = () => {
 
-    const {hola} = useAuthContext();
+    const {isChecking} = useAuthContext();
+
+    if(isChecking){
+        return <h1>Verificando...</h1>
+    }
 
     return (
         <>
             <h3>Login</h3>
-            <span>{hola}</span>
         </>
     )
 }
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,22 @@
-import { createContext, useContext, type PropsWithChildren } from "react";
+import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+
+
+// enum AuthStatus {
+//     'checking',
+//     'authenticated',
+//     'unauthenticated'
+// }
 
 interface AuthState {
-    hola: string;
+    status: 'checking' | 'authenticated' | 'unauthenticated';
+    token?: string;
+    user?: User,
+    isChecking: boolean
+}
+
+interface User {
+    name: string;
+    email: string;
 }
 
 export const AuthContext = createContext({} as AuthState);
@@ -9,8 +24,20 @@ export const AuthContext = createContext({} as AuthState);
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({children}: PropsWithChildren) => {
+
+    const [status, setStatus] = useState('checking');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setStatus('unauthenticated');
+        }, 2000)
+    }, [])
+
     return (
-        <AuthContext.Provider value={{hola: 'Mundo'}}>
+        <AuthContext.Provider value={{
+            status: 'checking',
+            isChecking: status === 'checking'
+        }}>
             {children}
         </AuthContext.Provider>
     )
